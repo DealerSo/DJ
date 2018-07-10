e2af7a6251c835b2c9f7889d82a6628e890a6ddf
Vincent <gw0683@163.com>
2018-07-10 14:48:22 +0800

    调用企业微信api,获取Access Token和调用发发送消息

--- dj-wechat/src/main/java/com/dj/wechat/util/WechatParamsUtil.java
+++ dj-wechat/src/main/java/com/dj/wechat/util/WechatParamsUtil.java
@@ -2,10 +2,10 @@ package com.dj.wechat.util;
 
 public class WechatParamsUtil {
     // 企业ID，唯一值
-    public static final String CORPID = "xxxxxx";
+    public static final String CORPID = "xxxxxxxxxx";
 
     //新消息应用
-    public static final String CONTACT_CORPSECRET = "xxxxxx";
+    public static final String CONTACT_CORPSECRET = "xxxxxxxxxx";
     public static final int CONTACT_AGENTID = 1000011;   // agentId
 
 }
